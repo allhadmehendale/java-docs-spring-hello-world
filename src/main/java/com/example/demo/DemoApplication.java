80580a9446450a5e005dd9302b39375419e78f05
allhad mehendale <allhad.mehendale@tomtom.com>
2023-03-14 15:24:14 +0530

    add default creds

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+import com.azure.core.util.BinaryData;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +22,17 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!";
+		DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder().build();
+		BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+				.endpoint("https://stvadfbdummyvaddev.storage.windows.net")
+				.credential(defaultCredential)
+				.buildClient();
+		BlobContainerClient feedback = blobServiceClient.getBlobContainerClient("feedback");
+		BlobClient blobClient = feedback.getBlobClient("0421cd5c-803e-11ed-85c0-e295c391af89.pb");
+		BinaryData binaryData = blobClient.downloadContent();
+
+		//0421cd5c-803e-11ed-85c0-e295c391af89.pb
+
+		return "Hello World!"+binaryData;
 	}
 }
